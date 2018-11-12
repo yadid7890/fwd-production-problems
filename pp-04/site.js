e2e980c5ea2ff92c61a2c40845727e9571107f04
Yadid Gutierrez Huerta <ygutierr@hawk.iit.edu>
2018-11-12 00:18:14 -0600

    Add code to change color of button; add js code for ring bell

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,22 @@
+(function(){
+  // Check to make sure essential features are supported
+  if (typeof document.querySelector === 'undefined') {
+    // No? Then bye!!!
+    return;
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+document.querySelector(".doorbell").addEventListener("click", ringDoorbell());
+}
+
+let button = document.querySelector("button");
+button.addEventListener("click", ringDoorbell());
+});
+
+window.addEventListener("keypress", event => {
+  if (event.key == "d") {
+    doorbell.play();
+  }
+});
